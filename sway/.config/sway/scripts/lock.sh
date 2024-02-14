1d5ecb42343adebc49c2423ec902452cbbf25ee8
viktorybloom <viktorsulejic@gmail.com>
2024-02-14 13:28:08 +1000

    sway lock updates

--- sway/.config/sway/scripts/lock.sh
+++ sway/.config/sway/scripts/lock.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 # Times the screen off and puts it to background
 swayidle -w \
-        timeout 600 'swaymsg "output * power off"' \
-            resume 'swaymsg "output * power on"' & 
-        before-sleep 'swaylock -C ~/.config/swaylock/config'
+    timeout 300 'swaylock -C ~/.config/swaylock/config' \  
+    timeout 600 'swaymsg "output * power off"' \
+    resume 'swaymsg "output * power on"' \
 
-# Locks the screen immediately
-swaylock -C ~/.config/swaylock/config
-# Kills last background task so idle timer doesn't keep running
-kill %%
 
